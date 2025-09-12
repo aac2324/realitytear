ed847574cd4f06959b544cde40a2eea21566a9cb
aac2324 <161326407+aac2324@users.noreply.github.com>
2025-09-12 17:19:48 +0200

    Navigationsbar

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title', 'Event Reviews')</title>
+</head>
+<body>
+    <nav>
+        <a href="{{ route('events.index') }}">Events</a> |
+        <a href="{{ route('hosts.index') }}">Hosts</a> |
+        <a href="{{ route('reviews.index') }}">Reviews</a> |
+        <a href="{{ route('users.index') }}">Users</a>
+    </nav>
+
+    <hr>
+
+    <div>
+        @yield('content')
+    </div>
+</body>
+</html>
+
